bfab3eb38ab57903f1e9aabba8567cbcaec4d27c
philthefox <43797286+philthefox@users.noreply.github.com>
2020-12-06 23:00:03 +0100

    Challenge 4 added nochmal

--- docs/00_scripts/challenge_4.R
+++ docs/00_scripts/challenge_4.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(lubridate)
+library(maps)
 
 # 2.0 DATA IMPORT ----
 
@@ -23,9 +24,17 @@ mortality_rate_tbl <- covid_data_wrangled_tbl %>%
   left_join(covid_data_wrangled_tbl) %>% 
   select(countriesAndTerritories, deaths_total, popData2019) %>% 
   distinct() %>% 
-  mutate(mortality_rate = deaths_total / popData2019 * 100)
+  mutate(mortality_rate = deaths_total / popData2019 * 100) %>% 
+  mutate(across(countriesAndTerritories, str_replace_all, "_", " ")) %>%
+  mutate(countriesAndTerritories = case_when(
+    countriesAndTerritories == "United Kingdom" ~ "UK",
+    countriesAndTerritories == "United States of America" ~ "USA",
+    countriesAndTerritories == "Czechia" ~ "Czech Republic",
+    TRUE ~ countriesAndTerritories))
 
 # 4.0 PLOT
+
+# 4.1 Cumulative Covid-19 cases
 
 covid_data_germany_tbl <- covid_data_wrangled_tbl %>% 
   filter(countriesAndTerritories == "Germany") %>% 
@@ -59,6 +68,28 @@ covid_data_germany_tbl %>%
   geom_line(data = covid_data_france_tbl, aes(date, cases_total)) +
   geom_line(data = covid_data_spain_tbl, aes(date, cases_total)) +
   geom_line(data = covid_data_usa_tbl, aes(date, cases_total))
+
+# 4.2 Mortality rate world map
+
+world <- map_data("world")
+
+plot <- mortality_rate_tbl %>%
+  left_join(world, by = c("countriesAndTerritories" = "region")) %>% 
+  ggplot(aes(x = long, y = lat, color = mortality_rate)) +
+  geom_map(aes(fill = mortality_rate), map = world)
+
+# I didn't manage to finish the last two plots, but I worked a whole week on
+# the challenges and learned a lot which I will be able to use in the following
+# two data science courses. I hope that I passed this course anyway.
+
+
+
+
+
+
+
+
+
 
 
 
